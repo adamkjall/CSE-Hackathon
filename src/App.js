b48b45cd878a2ad29f3d5bc36461d43a414abf94
Adam <bottarn.botvidsson@gmail.com>
2019-05-11 11:20:14 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import {
+  MDBBtn,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBView,
+  MDBMask
+} from "mdbreact";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -12,12 +19,19 @@ class App extends Component {
       <Router>
         <div>
           <NavBar />
-          <MDBRow className="mb-4">
-            <img
-              src="https://mdbootstrap.com/img/Others/documentation/1.jpg"
-              className="img-fluid"
-              alt=""
-            />
+          <MDBRow>
+            <MDBContainer>
+              <MDBView className="mt-2">
+                <img
+                  src="https://lh5.googleusercontent.com/6_KLr7jkcE4hK9f5O8rb7luqOWlL24d0tKGp7s97CxVDZ5Nk0Vqt8EzQY78rGKrlicJqzSnNfx_az8DaoIUVmO4iO9ipjxx3706Yp2ZhozWCuFY76afc_KfPcbxqannM_wF-du_k"
+                  class="img-fluid"
+                  alt="sample image"
+                />
+                <MDBMask className="flex-center" overlay="black-strong">
+                  <p className="white-text">Everything is awesome</p>
+                </MDBMask>
+              </MDBView>
+            </MDBContainer>
           </MDBRow>
 
           <MDBRow center style={{ height: "100vh" }}>
@@ -40,3 +54,8 @@ class App extends Component {
           <Footer />
         </div>
       </Router>
+    );
+  }
+}
+
+export default App;
