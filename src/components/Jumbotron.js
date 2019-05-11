a18fd170ed3914dd5c7b14973f52bb6d33bdb86c
Adam <bottarn.botvidsson@gmail.com>
2019-05-11 17:57:20 +0200

    Added text to the jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -11,31 +11,30 @@ import {
 
 const JumbotronPage = () => {
   return (
-
-          <MDBJumbotron style={{ padding: 0 }}>
-            <MDBCol
-              className="text-white text-center py-5 px-4"
-              style={{
-                backgroundImage: `url(https://lh5.googleusercontent.com/6_KLr7jkcE4hK9f5O8rb7luqOWlL24d0tKGp7s97CxVDZ5Nk0Vqt8EzQY78rGKrlicJqzSnNfx_az8DaoIUVmO4iO9ipjxx3706Yp2ZhozWCuFY76afc_KfPcbxqannM_wF-du_k)`
-              }}
-            >
-              <MDBCol className="py-5">
-                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">
-                  Create your beautiful website with MDBootstrap
-                </MDBCardTitle>
-                <p className="mx-5 mb-5">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Repellat fugiat, laboriosam, voluptatem, optio vero odio nam
-                  sit officia accusamus minus error nisi architecto nulla ipsum
-                  dignissimos. Odit sed qui, dolorum!
-                </p>
-                <MDBBtn outline color="white" className="mb-5">
-                  <MDBIcon icon="clone" className="mr-2" /> Read more
-                </MDBBtn>
-              </MDBCol>
-            </MDBCol>
-          </MDBJumbotron>
- 
+    <MDBJumbotron style={{ padding: 0 }}>
+  
+      <MDBCol
+        className="text-white text-center py-5 px-4"
+        style={{
+          backgroundImage: `url(https://lh5.googleusercontent.com/6_KLr7jkcE4hK9f5O8rb7luqOWlL24d0tKGp7s97CxVDZ5Nk0Vqt8EzQY78rGKrlicJqzSnNfx_az8DaoIUVmO4iO9ipjxx3706Yp2ZhozWCuFY76afc_KfPcbxqannM_wF-du_k)`
+        }}
+      >
+       
+        <MDBCol className="py-5">
+        
+          <MDBCardTitle className="display-4 pt-3 m-5 font-bold">
+            Start your tech career right now!
+          </MDBCardTitle>
+          <h4 className="mx-5 mb-5 px-5">
+          Sweden’s innovative and expansive IT sector is facing a future skills shortage, and education and immigration reforms are part of the remedy.
+           
+          </h4>
+          <MDBBtn outline color="white" className="mb-5">
+            <MDBIcon icon="clone" className="mr-2" /> Read more
+          </MDBBtn>
+        </MDBCol>
+      </MDBCol>
+    </MDBJumbotron>
   );
 };
 
